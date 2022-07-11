9d61c6990181b0b4c8a7061642978569a8a44315
hysg <hysg@flowix>
2022-07-11 19:36:38 +0800

    bump reflex-platform to v0.9.2.0
    Without this change building testserver failed:
    
        nix build -f travis.nix testserver -o server
    
    Failed with error:
    ```
    error: builder for '/nix/store/aidzka3zxdf51gqxafrhjrdlrqld7fsl-servant-reflex-0.3.6.drv' failed with exit code 1;
           last 10 log lines:
           >   die', called at libraries/Cabal/Cabal/Distribution/Simple/Configure.hs:950:20 in Cabal-2.4.0.1:Distribution.Simple.Configur       >   configureFinalizedPackage, called at libraries/Cabal/Cabal/Distribution/Simple/Configure.hs:460:12 in Cabal-2.4.0.1:Distribtion.Simple.Configure
           >   configure, called at libraries/Cabal/Cabal/Distribution/Simple.hs:596:20 in Cabal-2.4.0.1:Distribution.Simple
           >   confHook, called at libraries/Cabal/Cabal/Distribution/Simple/UserHooks.hs:67:5 in Cabal-2.4.0.1:Distribution.Simple.UserHoks
           >   configureAction, called at libraries/Cabal/Cabal/Distribution/Simple.hs:178:19 in Cabal-2.4.0.1:Distribution.Simple
           >   defaultMainHelper, called at libraries/Cabal/Cabal/Distribution/Simple.hs:115:27 in Cabal-2.4.0.1:Distribution.Simple
           >   defaultMain, called at /nix/store/4mdp8nhyfddh7bllbi7xszz7k9955n79-Setup.hs:2:8 in main:Main
           > Setup: Encountered missing dependencies:
           > reflex-dom-core >=0.7
           >
    ```
    
    Bumping reflex-platform t0 v0.9.2.0 fixes it.

--- nix/reflex-platform.nix
+++ nix/reflex-platform.nix
@@ -1,7 +1,8 @@
 let
-  rev = "1aba6f367982bd6dd78ec2fda75ab246a62d32c5" ;
+  rev = "v0.9.2.0";
+  sha256 = "sha256:16q1rq0rwi6l28fv46q8m0hvb9rxrzf574j865vaz04xy8d5p1ya";
 in import (builtins.fetchTarball
   {
     url = "https://github.com/reflex-frp/reflex-platform/archive/${rev}.tar.gz";
-    sha256 = "137vg6i8yy4lp6rz4wbvvhfcc9d9qf7j1b3fnng3r7sj63k7j664" ;
+    inherit sha256;
   }) {}
